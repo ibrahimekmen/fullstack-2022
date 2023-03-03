939c1117f265e1960734de4088b2e9f85bd55ab9
ibrahim <ibrahimekmentr70@gmail.com>
2023-03-03 15:45:30 +0300

    favorite like helper method and its unit test

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -14,9 +14,20 @@ const totalLikes = (blogs) => {
         ? 0
         : blogs.reduce(reducer, 0)
 }
+
+const favoriteBlog = (blogs) => {
+    const reducer = (max, current) => {
+        return (current.likes >= max.likes ) ? current : max
+    }
+
+    return blogs.length === 0
+        ? null
+        : blogs.reduce(reducer)
+}
   
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
   
