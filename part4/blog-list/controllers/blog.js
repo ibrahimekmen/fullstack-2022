672ae12d7bc381a5e5b56fb5f518f56398a9ef6d
ibrahim <ibrahimekmentr70@gmail.com>
2023-03-07 16:22:37 +0300

    linking blogs and users

--- part4/blog-list/controllers/blog.js
+++ part4/blog-list/controllers/blog.js
@@ -1,9 +1,10 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 const logger = require('../utils/logger')
 
 blogRouter.get('/', async(request, response) => {   
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate("user", {username: 1, name: 1})
     response.json(blogs)
 })
 
@@ -19,6 +20,7 @@ blogRouter.get('/:id', async(request, response) => {
 
 blogRouter.post('/', async(request, response) => {
     const body = request.body
+    const user = await User.findById(body.userId)
     if(!body.title || !body.url){
         response.status(400).send('Bad Request')
     }else{
@@ -26,10 +28,13 @@ blogRouter.post('/', async(request, response) => {
             title: body.title,
             author: body.author,
             url: body.url,
-            likes: body.likes || 0
+            likes: body.likes || 0,
+            user: user.id
         })
 
         const savedBlog = await blog.save()
+        user.blogs = user.blogs.concat(savedBlog._id)
+        await user.save()
         response.status(201).json(savedBlog) 
     }    
 })
