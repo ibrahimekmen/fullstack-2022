8d480e54bafa4d0fb14493dab635e3b90c2d4b69
ibrahim <ibrahimekmentr70@gmail.com>
2023-03-07 16:51:38 +0300

    login router and controller added

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -1,16 +1,19 @@
 const express = require('express')
 require('express-async-errors')
 const app = express()
-const blogRouter = require('./controllers/blog')
-const usersRouter = require('./controllers/users')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const blogRouter = require('./controllers/blog')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 const url = config.MONGODB_URI
 
