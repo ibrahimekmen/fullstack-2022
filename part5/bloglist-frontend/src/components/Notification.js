41b5de01419dfb1eeec6af3700e0032bb79da7e7
ibrahim <ibrahimekmentr70@gmail.com>
2023-03-10 18:18:46 +0300

    css added for notifications

--- part5/bloglist-frontend/src/components/Notification.js
+++ part5/bloglist-frontend/src/components/Notification.js
@@ -1,13 +1,22 @@
-const Notification = ({message}) => {
+const Notification = ({message, error=true}) => {
     if (message === null){
         return null
     }
 
+    if(error){
+        return (
+            <div className="error">
+                {message}
+            </div>
+        )
+    }
+
     return (
-        <div className="error">
+        <div className="message">
             {message}
         </div>
     )
+    
 }
 
 export default Notification
