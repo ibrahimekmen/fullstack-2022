47aaf9468b77c64d37ca8d0d283f2c8e181ad8ed
ibrahim <ibrahimekmentr70@gmail.com>
2023-03-15 20:36:34 +0300

    test route and cypress added

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -18,6 +18,11 @@ app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
+if (process.env.NODE_ENV === 'test') {
+    const testingRouter = require('./controllers/test')
+    app.use('/api/test', testingRouter)
+}
+
 const url = config.MONGODB_URI
 
 mongoose.set('strictQuery', false)
