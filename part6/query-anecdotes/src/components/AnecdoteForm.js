8816c24f78c0ee091eae44b9023fd0af33ec8de5
ibrahim <ibrahimekmentr70@gmail.com>
2023-04-04 23:07:56 +0300

    error handling for 5 char long and useContext err

--- part6/query-anecdotes/src/components/AnecdoteForm.js
+++ part6/query-anecdotes/src/components/AnecdoteForm.js
@@ -1,11 +1,17 @@
 import { useMutation, useQueryClient } from 'react-query'
 import { createAnecdote } from '../requests'
+import { useNotificationDispatch } from '../NotificationContext'
 
 const AnecdoteForm = () => {
+  const dispatch = useNotificationDispatch()
+  
   const queryClient = useQueryClient()
   const newAnecdoteMutation = useMutation(createAnecdote, {
     onSuccess: () => {
       queryClient.invalidateQueries('anecdotes')
+    },
+    onError: () => {
+      dispatch({type: 'ERROR_NOTIFICATION'})
     }
   })
   
@@ -14,6 +20,10 @@ const AnecdoteForm = () => {
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
     newAnecdoteMutation.mutate({content, votes: 0})
+    dispatch({type: 'NEW_NOTIFICATION', content})
+    setTimeout(() => {
+      dispatch({type: 'REMOVE_NOTIFICATION'})
+    }, 5000)
     console.log('new anecdote')
 }
 
@@ -21,7 +31,7 @@ const AnecdoteForm = () => {
     <div>
       <h3>create new</h3>
       <form onSubmit={onCreate}>
-        <input name='anecdote' minLength={5}/>
+        <input name='anecdote'/>
         <button type="submit">create</button>
       </form>
     </div>
