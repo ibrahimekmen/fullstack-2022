c3c3c3f47c6bc89133e1d9af73542fe7b9c25991
ibrahim <ibrahimekmentr70@gmail.com>
2023-04-04 23:08:09 +0300

    displaying notifications done

--- part6/query-anecdotes/src/components/Notification.js
+++ part6/query-anecdotes/src/components/Notification.js
@@ -1,16 +1,20 @@
+import { useNotificationValue } from "../NotificationContext"
+
 const Notification = () => {
+  const notificationMessage = useNotificationValue()
+  
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1,
-    marginBottom: 5
+    marginBottom: 5,
+    display: notificationMessage === '' ? 'none' : ''
   }
   
-  if (true) return null
 
   return (
     <div style={style}>
-      
+      {notificationMessage}
     </div>
   )
 }
